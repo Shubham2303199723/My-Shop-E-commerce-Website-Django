09d56a1579402d819baaa20ff47fe990a471079b
Shubham Gurjar <shubhamgurjar222@gmail.com>
2023-09-15 16:19:40 +0530

    Cart Product Count Work Started

--- Store/views/home.py
+++ Store/views/home.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect
 from Store.models.product import product
 from Store.models.category import category
 from django.views import View
+from django.http import HttpResponse
 
 
 class Index(View):
     def get(self, request):
+
+        cart = request.session.get('cart')
+        if not cart:
+            request.session.cart = {'cart'}
         products = None
         categories = category.get_all_categories()
         categoryID = request.GET.get('category')
@@ -16,11 +21,21 @@ class Index(View):
         data = {}
         data ['products'] = products
         data ['categories'] = categories
-        print(request.session.get('email'))
-        print(request.session.get('customer_id'))
         return render(request, "index.html", data)
+    
     def post (self, request):
         product = request.POST.get('product')
-        print(product)
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity: 
+                cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+        print('cart',request.session['cart'])
         return redirect('homepage')
         

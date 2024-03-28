280f02b099a461f8eb1879ca94f8610f6a8c6294
shubham gurjar <shubhamgurjar222@gmail.com>
2024-03-28 08:33:46 +0530

    Middleware concept done

--- Store/views/orders.py
+++ Store/views/orders.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from Store.models import Orders
+from Store.middlewares.auth import auth_middleware
+from django.utils.decorators import method_decorator
 
 class OrderCart(View):
+    @method_decorator(auth_middleware)
     def get(self, request):
         customer = request.session.get('customer')
         orders = Orders.get_orders_by_customer(customer)

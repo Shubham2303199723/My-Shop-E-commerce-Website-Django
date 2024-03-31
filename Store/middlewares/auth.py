f1b51ed0bfcb5e21d2e2e416da8188cd0f569dca
shubham gurjar <shubhamgurjar222@gmail.com>
2024-03-31 08:58:36 +0530

    Return URL

--- Store/middlewares/auth.py
+++ Store/middlewares/auth.py
@@ -4,8 +4,9 @@ from django.shortcuts import redirect
 def auth_middleware(get_response):
 
     def middleware(request):
+        return_url = request.META['PATH_INFO']
         if not request.session.get('customer'):
-            return redirect('login')
+            return redirect(f'login?return_url={return_url}')
         response = get_response(request)
         return response
 
